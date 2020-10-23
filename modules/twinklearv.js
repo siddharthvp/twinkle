d22d7ef44c02f6ec1eafd2c0f8c6b86ba3ba62de
Siddharth VP <siddharthvp@gmail.com>
2020-10-23 15:11:28 +0530

    arv: use OOUI title/user input widgets
    TitleInputWidget for AN3 page selection.
    UserInputWidget for SPI sockpuppeteer and sock selections. In case of socks (dyninput field), OOUI is available only for the first 2 sock fields which are shown by default.

--- modules/twinklearv.js
+++ modules/twinklearv.js
@@ -239,6 +239,7 @@ Twinkle.arv.callback.changeCategory = function (e) {
 				{
 					type: 'input',
 					name: 'sockmaster',
+					className: 'userInput',
 					label: 'Sockpuppeteer',
 					tooltip: 'The username of the sockpuppeteer (sockmaster) without the User:-prefix'
 				}
@@ -278,10 +279,11 @@ Twinkle.arv.callback.changeCategory = function (e) {
 					type: 'dyninput',
 					name: 'sockpuppet',
 					label: 'Sockpuppets',
+					className: 'userInput',
 					sublabel: 'Sock: ',
 					tooltip: 'The username of the sockpuppet without the User:-prefix',
 					min: 2
-				});
+				}); // XXX: OOUI conversion works only for the first 2 sock fields which are visible by default
 			work_area.append({
 				type: 'textarea',
 				label: 'Evidence:',
@@ -312,6 +314,7 @@ Twinkle.arv.callback.changeCategory = function (e) {
 			work_area.append({
 				type: 'input',
 				name: 'page',
+				className: 'titleInput',
 				label: 'Page',
 				tooltip: 'The page being reported'
 			});
@@ -440,6 +443,7 @@ Twinkle.arv.callback.changeCategory = function (e) {
 			old_area.parentNode.replaceChild(work_area, old_area);
 			break;
 	}
+	Twinkle.OOUIify($('[name=work_area]'));
 };
 
 Twinkle.arv.callback.evaluate = function(e) {
