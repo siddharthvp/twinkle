3bd95908313f62c2f332f0cd001674a050cae9b7
Siddharth VP <siddharthvp@gmail.com>
2021-02-19 16:18:50 +0530

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,13 @@
 
 module.exports = {
 	preset: 'jest-playwright-preset',
-	testEnvironment: './tests/integration/jest-environment.js',
 	testEnvironmentOptions: {
 		'jest-playwright': {
 			launchOptions: {
 				// switch this to false for debugging
 				headless: true
 			},
-			browsers: ['chromium', /*'firefox', 'webkit'*/]
+			browsers: ['chromium', 'firefox', 'webkit']
 		}
 	}
 }
